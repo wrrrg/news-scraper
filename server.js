80de5703966df1a6f48171decc0d132e4a34fa98
wrrrg <bill.gryta@gmail.com>
2018-03-22 20:57:40 -0500

    scrape redirect works

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get("/scrape", function(req, res) {
     });
 
     // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
+    res.redirect("/");
   });
 });
 
